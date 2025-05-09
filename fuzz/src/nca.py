c85d439fa832f6d25ba45b7286fabf62f7616bce
spinning <vuhoang.thduong@gmail.com>
2025-05-09 14:39:09 +0200

    update basemodel

--- fuzz/src/nca.py
+++ fuzz/src/nca.py
@@ -3,11 +3,67 @@
 
 import torch
 import torch.nn as nn 
-import torch.nn.functional as F
-from torch.utils.data import TensorDataset, DataLoader
-import numpy as np
+from torchviz import make_dot
 
-class NCA(nn.Module):
+class BaseModel(nn.Module):
+    def __init__(
+            self, 
+            device: str = 'cuda' if torch.cuda.is_available() else 'cpu', 
+            **kwargs
+    ) -> None:
+        super().__init__()
+        self.device = device
+        self.to(device)
+
+    def visualize_network(
+        self,
+        x: torch.Tensor,
+    ) -> None:
+        """
+        Visualize the model architecture and parameters.
+        """
+        # Do not double convert tensor to device
+        y = self(x)
+        dot = make_dot(y, params=dict(list(self.named_parameters()) + [('x', x)]))
+        dot.render(f"model_architecture_{self.__class__.__name__}", format="png")
+        
+    def get_model_summary(self) -> str:
+        """
+        Generate a summary of the model architecture and number of parameters.
+        """
+        model_summary = str(self)
+        return model_summary
+    
+    def get_num_parameters(self) -> int:
+        """
+        Calculate the total number of parameters in the model.
+        """
+        num_params = sum(p.numel() for p in self.parameters())
+        return num_params
+    
+    def check_dtype(self):
+        for name, param in self.named_parameters():
+            print(f"{name}: {param.dtype}")
+
+    def count_dtypes(self):
+        dtype_counts = {}
+        for param in self.parameters():
+            dtype = param.dtype
+            if dtype in dtype_counts:
+                dtype_counts[dtype] += 1
+            else:
+                dtype_counts[dtype] = 1
+        
+        for dtype, count in dtype_counts.items():
+            print(f"{dtype}: {count} parameters")
+
+    def get_model_size(self):
+        size_bytes = 0
+        for param in self.parameters():
+            size_bytes += param.nelement() * param.element_size()
+        return size_bytes / 1024**2  # Convert to MB
+
+class NCA(BaseModel):
     """
     Neighborhood Component Analysis (NCA) for dimensionality reduction
     and learning a distance metric.
