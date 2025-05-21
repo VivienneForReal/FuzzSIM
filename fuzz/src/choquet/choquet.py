e75a63be4bd00e439c12dccad21263028de0b7a7
VivienneForReal <vuhoang.thduong@gmail.com>
2025-05-21 06:48:35 +0200

    update choquet

--- fuzz/src/choquet/choquet.py
+++ fuzz/src/choquet/choquet.py
@@ -7,6 +7,7 @@ from typing import List
 
 from fuzz.src.capacity import Capacity
 from fuzz.src.choquet.classic import Choquet_classic
+from fuzz.src.choquet.d_choquet import d_Choquet_integral
 
 # Choquet
 class Choquet: 
@@ -14,7 +15,17 @@ class Choquet:
     Class to calculate the Choquet integral of a fuzzy set.
     """
     
-    def __init__(self, X: torch.Tensor, mu: List[Capacity], version: str = 'classic'):
+    def __init__(
+            self, 
+            X: torch.Tensor, 
+            mu: List[Capacity], 
+            version: str = 'classic',
+
+            # Parameters for d-Choquet integral
+            p: float = 1.0,
+            q: float = 1.0,
+            verbose: bool = False
+        ):
         """
         Initialize the Choquet class with two lists.
         
@@ -30,6 +41,8 @@ class Choquet:
         self.capacity = mu
         if version == "classic":
             self.choquet = self._get_classic(self.X, self.capacity)
+        elif version == "d-choquet":
+            self.choquet = self._get_d_choquet(self.X, self.capacity, p, q, verbose)
         else:
             raise ValueError("Unsupported Choquet version provided.")
     
@@ -40,3 +53,10 @@ class Choquet:
         :return: Choquet integral of the dataset
         """
         return Choquet_classic(X, mu, verbose=verbose)
+    
+    def _get_d_choquet(self, X, mu, p: float = 1.0, q: float = 1.0, verbose: bool = False) -> torch.float:
+        """
+        Compute the d-Choquet integral of the dataset
+        :return: d-Choquet integral of the dataset
+        """
+        return d_Choquet_integral(X, mu, p=p, q=q, verbose=verbose)
--- fuzz/src/choquet/d_choquet.py
+++ fuzz/src/choquet/d_choquet.py
@@ -28,4 +28,50 @@ def restricted_dissim(X1: torch.Tensor, X2: torch.Tensor, p: int = 1, q: int = 1
     
 
     # Case 3
-    return torch.norm(X1 - X2, p) ** (1 / q)
+    return torch.abs(X1.pow(p) - X2.pow(p)).pow(1/q)
+
+
+def d_Choquet_integral(X: torch.Tensor, mu: List, p: float = 1.0, q: float = 1.0, verbose: bool = False):
+    """
+    Compute the d-Choquet integral as defined in the paper, using the restricted dissimilarity measure
+    δ_p,q(x,y) = |x^p - y^p|^(1/q)
+    
+    Parameters:
+    - X: Input tensor representing a point in [0,1]^N
+    - mu: List of capacity values (must contain capacity for every subset)
+    - p, q: Parameters for the dissimilarity measure δ_p,q in (0, +∞)
+    - verbose: Whether to print intermediate values for debugging
+    
+    Returns:
+    - d-Choquet integral value
+    """
+    # print(f"X: {X}")
+    # Get permutation of the input dataset
+    permutation = enumerate_permute(X)
+    # print(f"Permutation: {permutation}")
+    # Define choquet sum
+    choquet = 0
+    # Define the observation
+    observation = X[0]
+
+    # Get max permutation (last element)
+    perm_max = permutation[0,-1]
+    # print(f"perm_max: {perm_max}")
+
+    for i in range(1,len(observation)):
+        minus = i-1
+        if minus == 0:
+            X_minus_1 = torch.zeros(1)
+        else: 
+            X_minus_1 = observation[minus]
+        
+        X_i = observation[i]
+        val_check = F.pad(perm_max[i:], (0, len(observation) - len(perm_max[i:])), value=-1)
+        # Compute the restricted dissimilarity
+        dissim = restricted_dissim(X_i, X_minus_1, p, q)
+        # Compute the choquet sum
+        choquet += locate_capacity(val_check, mu) * dissim
+
+        if verbose:
+            print(f"val_check: {val_check} - dissim: {dissim} - choquet: {choquet}")
+    return choquet
